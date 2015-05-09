b35fa41c46e3929adfc1413f48c6b7e5f395fde7
jipson7 <caleb.phillips@uoit.net>
2015-05-09 10:02:23 -0400

    Remove dummy binary, implement get request

--- src/cljlm_api/handler.clj
+++ src/cljlm_api/handler.clj
@@ -4,8 +4,6 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(def test-binary "google.binary")
-(def test-sentence "this is a test")
 
 (defn load-lm [binary]
   (LmReaders/readLmBinary binary))
@@ -15,14 +13,22 @@
         words (java.util.Arrays/asList array)]
     (.scoreSentence lm words)))
 
+
+(def test-binary "google.binary")
+(def test-sentence "this is a test")
+(def language-model (load-lm test-binary))
+
+
 (defn test-lm []
-  (let [lm (load-lm test-binary)
-        sentence test-sentence]
-    (format "%s = %.2f" sentence (compute-log-prob lm sentence))))
+  (let [sentence test-sentence]
+    (format "%s = %.2f" sentence (compute-log-prob language-model sentence))))
 
 (defroutes app-routes
   (GET "/" [] (test-lm))
+  (GET "/:sentence" [sentence] 
+    (pr-str (compute-log-prob language-model (str sentence))))
   (route/not-found "Not Found"))
+
 
 (def app
   (wrap-defaults app-routes site-defaults))
