6e83561755d87c08af9a722dd3f963198205621d
jipson7 <caleb.phillips@uoit.net>
2015-05-18 19:54:25 -0400

    Add functions to run and analyze Google binaries

--- src/cljlm_api/handler.clj
+++ src/cljlm_api/handler.clj
@@ -5,19 +5,30 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 
-(defn load-lm [binary]
+;Reads a binary language model
+(defn load-lm-binary [binary]
   (LmReaders/readLmBinary binary))
 
+;Reads a language model made with the google books data set
+(defn load-lm-google [binary vocab]
+  (LmReaders/readGoogleLmBinary binary vocab))
+
+;Provides access to Java Map that allows checking of raw counts
+(defn load-lm-map [binary vocab]
+  (LmReaders/readNgramMapFromBinary binary vocab))
+
+
+;GIves a logarthmic probability of a sentence given the language model. Sentence is passed as a single string
 (defn compute-log-prob [lm sentence]
   (let [array (.split (.trim sentence) "\\s+")
         words (java.util.Arrays/asList array)]
     (.scoreSentence lm words)))
 
 
-(def test-binary "google.binary")
+(def test-binary "language-models/google/english/eng.blm")
+(def test-vocab "language-models/google/english/vocab_cs")
 (def test-sentence "this is a test")
-(def language-model (load-lm test-binary))
-
+(def language-model (load-lm-google test-binary test-vocab))
 
 (defn test-lm []
   (let [sentence test-sentence]
