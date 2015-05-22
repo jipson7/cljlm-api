b6f60131b16bd9a89ce890132ac1dfef9fa6e081
jipson7 <caleb.phillips@uoit.net>
2015-05-22 17:10:08 -0400

    Add Java Option to allow for 10g of heap space

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+  :jvm-opts ["-Xmx10g"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [berkeley/nlp "1.0.0"]
                  [compojure "1.3.1"]
